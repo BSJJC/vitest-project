38b9d5d9fda3b6ec2955e8ad3146dd5bc8fc1534
BSJJC <805086021@qq.com>
2023-02-13 14:56:11 +0800

    bench done

--- src/__test__/bench.bench.ts
+++ src/__test__/bench.bench.ts
@@ -1,5 +1,8 @@
 import { bench } from 'vitest';
 
+
+
+/// API bench
 bench(
   'normal sorting',
   () => {
@@ -9,9 +12,25 @@ bench(
   { time: 1000 }
 );
 
+
+/// API bench.skip
 bench.skip(
   "skipp this test", () => {
     const x = [2, 5, 4, 1, 3,];
     x.sort((a, b,) => b - a)
   }
-)
+);
+
+
+/// API bench.only
+// bench.only(
+  bench(
+  "only this bench test will be excuted", () => {
+    const x = [1, 5, 4, 2, 3];
+    x.sort((a, b) => a - b);
+  }
+);
+
+
+/// API bench.todo
+bench.todo("unimplemented  test");
